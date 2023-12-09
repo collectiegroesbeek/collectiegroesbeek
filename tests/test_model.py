41faf58e2415d5d89539cf262a4085dd98505471
Frank <33519926+Conengmo@users.noreply.github.com>
2023-12-09 12:04:43 +0100

    fix tests?

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,11 @@
-from collectiegroesbeek.model import CardNameDoc
+from collectiegroesbeek.model import create_year
 
 
 class TestCardNameIndex:
     @staticmethod
     def test_create_year():
-        assert CardNameDoc.create_year("1513") == 1513
-        assert CardNameDoc.create_year("") is None
-        assert CardNameDoc.create_year("1513-04-01") == 1513
-        assert CardNameDoc.create_year("1513-4-1") == 1513
-        assert CardNameDoc.create_year("1316-11-21 en 1317-04-21") == 1316
+        assert create_year("1513") == 1513
+        assert create_year("") is None
+        assert create_year("1513-04-01") == 1513
+        assert create_year("1513-4-1") == 1513
+        assert create_year("1316-11-21 en 1317-04-21") == 1316
