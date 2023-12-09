8971542de22832a62b9671e48fb411b331bf2f02
Frank <33519926+Conengmo@users.noreply.github.com>
2023-12-09 12:30:52 +0100

    move elasticsearch connection to app start

--- collectiegroesbeek/__init__.py
+++ collectiegroesbeek/__init__.py
@@ -1,10 +1,5 @@
 import flask
-from dotenv import dotenv_values
-
 
 app = flask.Flask(__name__)
-
-_config = dotenv_values(".env")
-app.config["elasticsearch_host"] = _config["elasticsearch_host"]
 
 from . import routes as view  # noqa E402
