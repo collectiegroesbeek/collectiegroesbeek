4185598fd0f864f5ab4031a20b31ab565c166438
Conengmo <33519926+Conengmo@users.noreply.github.com>
2024-10-29 16:59:02 +0100

    fix git gone wrong

--- collectiegroesbeek/controller.py
+++ collectiegroesbeek/controller.py
@@ -158,8 +158,8 @@ def get_page_range(hits_total: int, page: int, cards_per_page: int) -> List[int]
     return list(range(first_item, last_item + 1))
 
 
-    s = elasticsearch_dsl.Search(index="*")
 def get_bronnen_list(min_doc_count: int) -> List[dict]:
+    s = elasticsearch_dsl.Search(index="*")
     s.aggs.bucket(
         name="op_bron",
         agg_type="terms",
