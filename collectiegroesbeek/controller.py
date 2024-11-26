c4c25c205fa2fb6c80ab6c02861e89f313244ccc
Conengmo <33519926+Conengmo@users.noreply.github.com>
2024-11-26 10:08:44 +0100

    verwijder ongebruikte code

--- collectiegroesbeek/controller.py
+++ collectiegroesbeek/controller.py
@@ -169,41 +169,6 @@ def get_page_range(hits_total: int, page: int, cards_per_page: int) -> List[int]
     return list(range(first_item, last_item + 1))
 
 
-def get_grouped_bronnen() -> dict[str, int]:
-    bronnen_list = get_bronnen_list()
-    return _group_bronnen(bronnen_list)
-
-
-def get_bronnen_list() -> List[dict]:
-    s = elasticsearch_dsl.Search(index="*")
-    s.aggs.bucket(
-        name="op_bron",
-        agg_type="terms",
-        field="bron_prefix",
-        # get the 10k largest buckets, sort later
-        size=10_000,
-    )
-    res = s.execute()
-    bron_list: list[dict] = list(res.aggregations["op_bron"].buckets)
-    bron_list = sorted(bron_list, key=lambda x: x["key"])
-    bron_list = [bron_item for bron_item in bron_list if bron_item["key"]]
-    # items of the form: {'key': 'Aa', 'doc_count': 117}
-    return bron_list
-
-
-def _group_bronnen(bronnen_list: list[dict]) -> dict[str, int]:
-    out: dict[str, int] = defaultdict(lambda: 0)
-    for bron in bronnen_list:
-        key = bron["key"]
-        key = re.sub(r" no [\d\s,]+$", "", key)
-        key = re.sub(r" \d+-\d+$", "", key)
-        key = re.sub(r" \d+$", "", key)
-        if key.isdigit():
-            continue
-        out[key] += bron["doc_count"]
-    return out
-
-
 def get_suggestions(keywords: Iterable[str]):
     tokens = [token for token in keywords if not token.isdigit()]
     s = CardNameDoc.search()
